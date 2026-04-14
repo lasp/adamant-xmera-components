fe4137a5f3a6807a0fc0539982fee7bf3e8c633b
patrick kenneally <patrick.kenneally@lasp.colorado.edu>
2026-04-14 12:12:05 -0600

    Add css_comm unit test with zero-Chebyshev identity test case
    Adapted from C++ test ZeroChebyIsIdentity: with all zero Chebyshev
    coefficients, output equals input/maxSensorValue clamped to [0, 1].
    Includes tester with data dependency mock, data product history
    capture, and parameter staging/update helpers.

---
+++ src/components/css_comm/test/css_comm_tests-implementation.ads
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------
+-- Css_Comm Tests Spec
+--------------------------------------------------------------------------------
+
+-- Unit tests for the Css Comm component
+package Css_Comm_Tests.Implementation is
+
+   -- Test data and state:
+   type Instance is new Css_Comm_Tests.Base_Instance with private;
+   type Class_Access is access all Instance'Class;
+
+private
+   -- Fixture procedures:
+   overriding procedure Set_Up_Test (Self : in out Instance);
+   overriding procedure Tear_Down_Test (Self : in out Instance);
+
+   -- With zero Chebyshev coefficients, output equals input/maxSensorValue clamped to
+   -- [0, 1].
+   overriding procedure Test_Zero_Cheby_Is_Identity (Self : in out Instance);
+
+   -- Test data and state:
+   type Instance is new Css_Comm_Tests.Base_Instance with record
+      null;
+   end record;
+end Css_Comm_Tests.Implementation;
